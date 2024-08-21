d89a64a83364ca1cc0f2914855460fe5355440a0
Pavel Gorin <moyoblin88@yandex.ru>
2024-08-21 17:36:31 +0300

    part2

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -2,27 +2,32 @@ import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
+import clsx from 'clsx';
+
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-type Props = {
-	openStatus?: () => void;
-	open?: boolean;
+type ArrowButtonProps = {
+	state: boolean;
+	toggleState: OnClick;
 };
 
-export const ArrowButton = ({ openStatus, open }: Props) => {
+export const ArrowButton = (props: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			onClick={props.toggleState}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container + (open ? ` ${styles.container_open}` : '')}
-			onClick={openStatus}>
+			className={clsx(
+				styles.container,
+				props.state ? styles.container_open : ''
+			)}>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={styles.arrow + (open ? ` ${styles.arrow_open}` : '')}
+				className={clsx(styles.arrow, props.state ? styles.arrow_open : '')}
 			/>
 		</div>
 	);
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { StrictMode, CSSProperties, useState, FormEvent } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { ArticleStateType, defaultArticleState } from './constants/articleProps';
+import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,21 +13,45 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [appState, setAppState] = useState<ArticleStateType>(defaultArticleState);
+	const [pageState, setPageState] = useState(defaultArticleState);
+	const [formState, setFormState] = useState(defaultArticleState);
+
+	const formReset = () => {
+		setFormState(defaultArticleState);
+		setPageState(defaultArticleState);
+	};
+
+	const formSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		setPageState({
+			...pageState,
+			fontFamilyOption: formState.fontFamilyOption,
+			fontSizeOption: formState.fontSizeOption,
+			fontColor: formState.fontColor,
+			contentWidth: formState.contentWidth,
+			backgroundColor: formState.backgroundColor,
+		});
+	};
 
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': appState.fontFamilyOption.value,
-					'--font-size': appState.fontSizeOption.value,
-					'--font-color': appState.fontColor.value,
-					'--container-width': appState.contentWidth.value,
-					'--bg-color': appState.backgroundColor.value,
+					'--font-family': pageState.fontFamilyOption.value,
+					'--font-size': pageState.fontSizeOption.value,
+					'--font-color': pageState.fontColor.value,
+					'--container-width': pageState.contentWidth.value,
+					'--bg-color': pageState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm appState={appState} setAppState={setAppState} />
+			<ArticleParamsForm
+				formState={formState}
+				setFormState={setFormState}
+				formReset={formReset}
+				formSubmit={formSubmit}
+			/>
 			<Article />
 		</div>
 	);
